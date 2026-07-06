ce3ff5491c681da6d7c128285c5a42948195cf8c
raj <rajshah97120@gmail.com>
2026-07-06 17:09:59 +0530

    update form changes

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -5,28 +5,34 @@ const Display = ({isLoggedIn,setIsLoggedIn}) => {
     <div>
        {isLoggedIn ? <p>You are logged in!</p> : 
        
-        <div>
-        <div>
-       <label htmlFor="username">Username</label>
-        <input 
-            id="username"
-            name='username'
-            type="text"
-        />
-        </div>
-
-        <div>
-       <label htmlFor="password">Password</label>
-        <input 
-            id="password"
-            name='password'
-            type="password"
-        />
-        </div>
-
-        <button onClick={() => setIsLoggedIn(!isLoggedIn)}>Login</button>
-        
-        </div>
+       <form
+       onSubmit={(e) => {
+         e.preventDefault();
+         setIsLoggedIn(true);
+       }}
+     >
+       <div>
+         <label htmlFor="username">Username</label>
+         <input
+           id="username"
+           name="username"
+           type="text"
+         />
+       </div>
+     
+       <div>
+         <label htmlFor="password">Password</label>
+         <input
+           id="password"
+           name="password"
+           type="password"
+         />
+       </div>
+     
+       <button type="submit">
+         Login
+       </button>
+     </form>
        
        }
     </div>
